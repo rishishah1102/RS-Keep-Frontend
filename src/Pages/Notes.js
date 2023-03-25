1179b9fded0e577be66544d40eef890e1ba83dba
rishishah1102 <rishi.shah545@gmail.com>
2023-03-25 18:37:19 +0530

    removed logs from notes.js

--- src/Pages/Notes.js
+++ src/Pages/Notes.js
@@ -18,11 +18,7 @@ export default function Notes() {
     useEffect(() => {
         const getData = async () => {
             let response = await axios.get('https://rs-keepbackend.onrender.com/notes', {headers: {Authorization: localStorage.getItem('token')}});
-            console.log(response);
             if (response.status === 200) {
-                if (response.data.notes === undefined) {
-                    return;
-                }
                 addItem(response.data.notes);
             } else {
                 alert("Server is down, Please try again later!");
